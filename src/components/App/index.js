4dc9e2978d8497a42b64364495140a2694041301
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-05-30 21:39:25 -0300

    Added TopBar y TabBar components and change body css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import TabBar from "../TabBar";
+import TopBar from "../TopBar";
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <TopBar />
+      <TabBar />
+    </div>
+  );
 }
 
 export default App;
