b49f579d3948e159e3b7b68b01829c79c44f8d2d
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-06-04 21:40:23 -0300

    Added infinte-scroll dependency

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,51 +1,49 @@
-import React, { Component } from 'react'
-import Title from "../../components/Title"
-import Grilla from "../../components/Grilla"
-import dayjs from 'dayjs'
-import LoadingLine from "../../components/LoadingLine"
-
+import React, { Component } from "react";
+import Title from "../../components/Title";
+import Grilla from "../../components/Grilla";
+import dayjs from "dayjs";
+import LoadingLine from "../../components/LoadingLine";
 
 class Home extends Component {
-    constructor() {  
-        super()
-        this.state = {
-            noticias: null,
-            loading: false
-        }
+  constructor() {
+    super();
+    this.state = {
+      noticias: null,
+      loading: false
+    };
+  }
+
+  setLoading = value => {
+    this.setState({
+      loading: value
+    });
+  };
+
+  componentDidMount = async () => {
+    let date = dayjs().format("YYYY-MM-DD");
+    try {
+      this.setLoading(true);
+      let data = await fetch(`https://api.canillitapp.com/latest/${date}`);
+      data = await data.json();
+      this.setState({
+        noticias: data
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setLoading(false);
     }
+  };
 
-    setLoading = ( value ) => {
-        this.setState({
-            loading: value
-        })
-    }
-
-    componentDidMount = async () => {
-        let date = dayjs().format('YYYY-MM-DD')
-        try {
-            this.setLoading(true)
-            let data = await fetch(`https://api.canillitapp.com/latest/${date}`)
-            data = await data.json()
-            this.setState( {
-                noticias: data.slice(0,27)
-            } )
-        } catch (error) {
-            console.log(error);
-        } finally {
-            this.setLoading(false);
-        }
-
-      }
-
-    render() {
-        return (
-            <div>
-                {this.state.loading ? <LoadingLine /> : null}
-                <Title title="Hoy" />
-                <Grilla noticias={this.state.noticias}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {this.state.loading ? <LoadingLine /> : null}
+        <Title title="Hoy" />
+        <Grilla noticias={this.state.noticias} />
+      </div>
+    );
+  }
 }
 
 export default Home;
