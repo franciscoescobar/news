f50f7cc7558cb78953ea93c49fea37fceb5c6d18
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-06-21 10:24:11 -0300

    Updates

---
+++ precache-manifest.b5c788c537c82bad734ab569e3354444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451c76639e139a3b37573a564df8e217",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "f09097773e58a9dd224c",
+    "url": "/news/static/css/main.227c5e94.chunk.css"
+  },
+  {
+    "revision": "f303b7d0d173d154435d",
+    "url": "/news/static/js/2.e93b1d93.chunk.js"
+  },
+  {
+    "revision": "f09097773e58a9dd224c",
+    "url": "/news/static/js/main.c6cc7be5.chunk.js"
+  },
+  {
+    "revision": "b6263ef5dad59d00f0a9",
+    "url": "/news/static/js/runtime~main.726e687c.js"
+  }
+]);
